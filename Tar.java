66561701ea257d8272d4f94bbb988f59dc2f5afe
eivycor <63749795+eivycor@users.noreply.github.com>
2020-05-05 21:18:45 -0500

    Add files via upload

---
+++ Tar.java
@@ -0,0 +1,34 @@
+package pro2;
+
+import java.util.Scanner;
+
+public class Tar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 Scanner scan= new Scanner(System.in);
+		 int num;
+			System.out.println("ingrese un numero");
+		 num= scan.nextInt();
+	
+		if (num<0) {
+		System.out.println("el numeroes negativo");}
+		else if(num>0) {
+		System.out.println("el numero es positivo");}
+		else if (num==0) {
+			System.out.println("el numero nulo");}
+		}
+}
+	
+	
+		
+		
+	
+	 
+		
+	
+		
+	
+
+
