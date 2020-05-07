23765e3c434197647608b07d04904778f125a85f
eivycor <63749795+eivycor@users.noreply.github.com>
2020-05-07 14:23:18 -0500

    Add files via upload

---
+++ Reto31.java
@@ -0,0 +1,16 @@
+package mm;
+import java.util.Scanner;
+public class Reto31 {
+static int num;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scan=new Scanner(System.in);
+System.out.println("ingresa un numero");
+num=scan.nextInt();
+if (num>0) {
+	System.out.println("positivo");
+}
+else System.out.println("negativo");}
+
+}
+-2
