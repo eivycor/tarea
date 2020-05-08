2546ec38446bab3a7192370b549ca1bd7526cb17
eivycor <63749795+eivycor@users.noreply.github.com>
2020-05-08 15:30:01 -0500

    Add files via upload

--- Sobre.java
+++ Sobre.java
@@ -13,16 +13,21 @@ public class Sobre {
 		switch (nota) {
 		case 0:
 			System.out.println("cero");
+			break;
 		case 1:
 			System.out.println("uno");
+			break;
 		case 2:
 			System.out.println("dos");
 		case 3:
 			System.out.println("tres");
+			break;
 		case 4:
 			System.out.println("cuatro");
+			break;
 		case 5:
 			System.out.println("cinco");
+			break;
 		case 6:
 			System.out.println("seis");
 			break;
@@ -31,10 +36,13 @@ public class Sobre {
 			break;
 		case 8:
 			System.out.println("ocho");
+			break;
 		case 9:
 			System.out.println("nueve");
+			break;
 		case 10:
 			System.out.println("diez");
+			break;
 
 		}
 	}
