eef54a265a8afefb556e2a16ddb91b5333cb3e51
eivycor <63749795+eivycor@users.noreply.github.com>
2020-05-21 18:06:47 -0500

    Add files via upload

---
+++ T.java
@@ -0,0 +1,19 @@
+package tarea6;
+
+public class T {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n, su ;
+		n = 1;
+su=0;
+		while (n < 40) {
+			;
+			n += 3;
+			System.out.println(n);
+			su = su + n;
+		}
+		System.out.println(" La suma es: " + su);
+
+	}
+}
