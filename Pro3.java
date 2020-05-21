e96109bc6a46a723066fce4d62c4b6f07d61ad31
eivycor <63749795+eivycor@users.noreply.github.com>
2020-05-21 18:07:31 -0500

    Add files via upload

---
+++ Pro3.java
@@ -0,0 +1,82 @@
+package repet;
+import java .util.Scanner;
+public class Pro3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	int n = 1;
+	int n2 = 1;
+	int n3 = 1;
+	int n4 = 1;
+	int n5 = 1;
+	int n6 = 1;
+	int n7 = 1;
+	int n8 = 1;
+	int n9 = 1;
+	int n10 = 1;
+
+	while (n < 10) {
+		System.out.print(n);
+		n++;
+	}
+	System.out.print("\n");
+
+	while (n < 9) {
+
+		System.out.print(n2);
+		n2++;
+	}
+	System.out.print("\n");
+
+	while (n3 < 8) {
+		System.out.print(n3);
+		n3++;
+	}
+	System.out.print("\n");
+
+	while (n4 < 7) {
+		System.out.print(n4);
+		n4++;
+	}
+	System.out.print("\n");
+
+	while (n5 < 6) {
+		System.out.print(n5);
+		n5++;
+	}
+	System.out.print("\n");
+
+	while (n6 < 5) {
+		System.out.print(n6);
+		n6++;
+	}
+	System.out.print("\n");
+
+	while (n7 < 4) {
+		System.out.print(n7);
+		n7++;
+	}
+	System.out.print("\n");
+
+	while (n8 < 3) {
+		System.out.print(n8);
+		n8++;
+	}
+	System.out.print("\n");
+
+	while (n9 < 2) {
+		System.out.print(n9);
+		n9++;
+	}
+	System.out.print("\n");
+
+	while (n10 < 1) {
+		System.out.print(n10);
+		n10++;
+	}
+	}
+}
+
+
+
